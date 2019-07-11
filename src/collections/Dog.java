5149d7eb09a92634b53f1ec74e3ad27e8153118c
mzevallos <38415054+mzevallos@users.noreply.github.com>
2019-07-11 15:14:39 -0400

    Add files via upload

--- src/collections/Dog.java
+++ src/collections/Dog.java
@@ -21,6 +21,39 @@ public class Dog
 	public double getTopSpeed() {return topSpeed;}
 
 	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + age;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		long temp;
+		temp = Double.doubleToLongBits(topSpeed);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Dog other = (Dog) obj;
+		if (age != other.age)
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (Double.doubleToLongBits(topSpeed) != Double.doubleToLongBits(other.topSpeed))
+			return false;
+		return true;
+	}
+
+	@Override
 	public String toString() {
 		return "Dog [name=" + name + ", age=" + age + ", topSpeed=" + topSpeed + "]";
 	}	
